9c16eba90840fbec646c31ca6eedc043e6410ad7
Brian Downs <brian.downs@gmail.com>
2016-03-02 11:00:34 -0700

    beginnings of file processing unit tests

---
+++ files_test.go
@@ -0,0 +1,20 @@
+package scaniigo
+
+import (
+	"testing"
+)
+
+// TestRetrieveProcessedFile
+func TestRetrieveProcessedFile(t *testing.T) {
+	t.Parallel()
+}
+
+// TestProcessFileSync
+func TestProcessFileSync(t *testing.T) {
+	t.Parallel()
+}
+
+// TestProcessFileAsync
+func TestProcessFileAsync(t *testing.T) {
+	t.Parallel()
+}
