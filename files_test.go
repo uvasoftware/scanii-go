b7ccb5d170e3c69280686130a5cfd2101b224b55
Brian Downs <brian.downs@gmail.com>
2016-03-06 20:20:01 -0700

    updating unit tests

--- files_test.go
+++ files_test.go
@@ -5,9 +5,30 @@ import (
 	"testing"
 )
 
+var localFile = "scan_files/test_binary"
+var remoteFile = "https://github.com/uvasoftware/scanii-go/scan_files/test_binary"
+
 // TestRetrieveProcessedFile
 func TestRetrieveProcessedFile(t *testing.T) {
 	t.Parallel()
+}
+
+// TestValidate
+func TestValidate(t *testing.T) {
+	t.Parallel()
+	pfp := &ProcessFileParams{
+		File: localFile,
+	}
+	rfap := &RemoteFileAsyncParams{
+		Location: remoteFile,
+	}
+	var testParams []Validator
+	testParams = append(testParams, pfp, rfap)
+	for _, i := range testParams {
+		if err := Validate(i); err != nil {
+			t.Error(err)
+		}
+	}
 }
 
 // TestProcessFileSync
@@ -45,7 +66,7 @@ func TestProcessFileAsync(t *testing.T) {
 		t.Error(err)
 	}
 	pfp := &ProcessFileParams{
-		File: "scan_files/test_binary",
+		File: localFile,
 	}
 	pfr, err := c.ProcessFileSync(pfp)
 	if err != nil {
@@ -68,7 +89,7 @@ func TestProcessRemoteFileAsync(t *testing.T) {
 		t.Error(err)
 	}
 	rfap := &RemoteFileAsyncParams{
-		Location: "https://github.com/uvasoftware/scanii-go/scan_files/test_binary",
+		Location: remoteFile,
 	}
 	pfr, err := c.ProcessRemoteFileAsync(rfap)
 	if err != nil {
